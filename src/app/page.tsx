0fc2102593fa418936505d7ddf4a493a7c1f79a0
Tammy Liu <tammy.liu.x@gmail.com>
2024-07-05 00:36:33 -0400

    Add screen size responsiveness for h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,39 +46,39 @@ function Title() {
   const goalsUrl = "https://asana.com/features/goals-reporting/goals";
 
   return (
-    <header className="flex w-full justify-center">
-      <h1 className="flex">
-        <p>
-          Tammy is building{" "}
-          <CustomLink className="underline underline-offset-4" href={goalsUrl}>
-            Goals at Asana
-          </CustomLink>
-        </p>
-      </h1>
-    </header>
+    <h1 className="flex">
+      <p>
+        Tammy is building{" "}
+        <CustomLink className="underline underline-offset-4" href={goalsUrl}>
+          Goals at Asana
+        </CustomLink>
+      </p>
+    </h1>
   );
 }
 
 function SubTitle() {
   const figureSkatingUrl = "https://www.instagram.com/tamz.on.ice";
   return (
-    <div className="flex w-full justify-center mb-28">
-      <h4>
+    <h4 className="flex">
+      <p>
         senior software engineer by daylight ☀️,{" "}
         <CustomLink className="underline" href={figureSkatingUrl}>
           figure skater
         </CustomLink>{" "}
         by moonlight &#127769;
-      </h4>
-    </div>
+      </p>
+    </h4>
   );
 }
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between">
-      <Title />
-      <SubTitle />
+    <main className="flex flex-col items-center justify-between space-y-4 p-10">
+      <header className="flex flex-col w-full justify-center space-y-8">
+        <Title />
+        <SubTitle />
+      </header>
       <SocialsSection />
     </main>
   );
